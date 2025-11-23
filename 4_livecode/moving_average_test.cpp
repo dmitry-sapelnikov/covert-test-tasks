caf3e02abc827a719cb4c8b8f1d6194888fa15e9
Dmitry Sapelnikov <dmitry.sapelnikov@gmail.com>
2025-11-23 22:23:39 +0100

    Minor fixes

--- 4_livecode/moving_average_test.cpp
+++ 4_livecode/moving_average_test.cpp
@@ -3,34 +3,33 @@
 
 namespace
 {
-
-	/// Simple function to check equality of two floating-point numbers
+	/// Checks equality of two floating-point numbers
 	void check_equal(
 		double actual,
 		double expected,
-		double epsilon = 1e-6)
+		double tolerance = 1e-6)
 	{
-		if (std::abs(expected - actual) > epsilon)
+		if (std::abs(expected - actual) > tolerance)
 		{
 			std::cerr << "Values are not equal: ";
 			std::cerr << "expected " << expected << " != actual " << actual << "\n";
 		}
 	}
 
-	// end of anonymous namespace
+// end of anonymous namespace
 }
 
 /// Test the MovingAverage class
 int main()
 {
+	std::cout << "Test detection of invalid values with check_equal() :\n";
+	check_equal(0.1 + 0.2, 0.5); // should report error
+
+	std::cout << "\nTesting MovingAverage class:\n";
 	// time:    0    1    2    3    4    5    6    7    8    9   10
 	// value:   1    1    1    2    2    3    3    3    4    5    6
 	// average: 1            5/4       9/5           15/5 18/5 21/5
 
-	std::cout << "Test detection of invalid values iwth check_equal() :\n";
-	check_equal(0.1 + 0.2, 0.5); // should report error
-
-	std::cout << "\nTesting MovingAverage class:\n";
 	moving_average::MovingAverage<uint64_t, double> ma(5);
 	check_equal(ma.add_event(0, 1.0), 1.0);
 	check_equal(ma.add_event(3, 2.0), 5.0 / 4.0);
